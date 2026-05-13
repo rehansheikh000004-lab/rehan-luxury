d95e32b39cd4ed22cb06919d70ddb69c21ece929
rehansheikh000004-lab <rehansheikh000004@gmail.com>
2026-05-13 19:09:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,108 @@
-/* REVEAL ANIMATION */
+/* =========================
+   LOADER
+========================= */
 
-const reveals = document.querySelectorAll(".reveal");
+window.addEventListener("load", () => {
+
+  const loader =
+    document.querySelector(".loader");
+
+  setTimeout(() => {
+
+    loader.classList.add("hidden");
+
+  }, 1800);
+
+});
+
+/* =========================
+   REVEAL ANIMATION
+========================= */
+
+const reveals =
+  document.querySelectorAll(".reveal");
 
 window.addEventListener("scroll", () => {
 
   reveals.forEach((el) => {
 
-    const top = el.getBoundingClientRect().top;
+    const top =
+      el.getBoundingClientRect().top;
 
     if (top < window.innerHeight - 100) {
+
       el.classList.add("active");
+
     }
 
   });
 
 });
 
-/* BUTTON EFFECT */
+/* =========================
+   BUTTON EFFECT
+========================= */
 
-const button = document.querySelector(".cta");
+const button =
+  document.querySelector(".cta");
 
 button.addEventListener("mouseenter", () => {
+
   button.style.letterSpacing = "2px";
+
 });
 
 button.addEventListener("mouseleave", () => {
+
   button.style.letterSpacing = "0px";
+
 });
+
+/* =========================
+   MOUSE GLOW
+========================= */
+
+const glow =
+  document.querySelector(".cursor-glow");
+
+document.addEventListener("mousemove", (e) => {
+
+  glow.style.left = e.clientX + "px";
+
+  glow.style.top = e.clientY + "px";
+
+});
+
+/* =========================
+   PARTICLES
+========================= */
+
+const particles =
+  document.querySelector(".particles");
+
+for (let i = 0; i < 60; i++) {
+
+  const particle =
+    document.createElement("span");
+
+  const size =
+    Math.random() * 4 + 2;
+
+  particle.style.width =
+    `${size}px`;
+
+  particle.style.height =
+    `${size}px`;
+
+  particle.style.left =
+    `${Math.random() * 100}%`;
+
+  particle.style.animationDuration =
+    `${Math.random() * 10 + 5}s`;
+
+  particle.style.animationDelay =
+    `${Math.random() * 5}s`;
+
+  particles.appendChild(particle);
+
+}
